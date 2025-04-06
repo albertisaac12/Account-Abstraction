e46bb5ebf8e0e561d0f65cc19c5d852f51da6dbd
abhi <abhiodf9@gmail.com>
2025-04-06 14:28:23 +0530

    Fixed Signature Error

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -38,6 +38,7 @@ contract SendPackedUserOp is Script {
         uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         if (block.chainid == 31337) {
             (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+            userOp.signature = abi.encodePacked(r, s, v);
         } else {
             // 3. Sign it and return it
             (v, r, s) = vm.sign(config.account, digest);
